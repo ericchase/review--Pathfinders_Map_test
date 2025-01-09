570c7c5751eca33a592a9be275b2336b562ed0c8
ericchase <github@eric.addymail.com>
2025-01-09 11:00:40 -0600

    add svg rectangle editing
    Signed-off-by: ericchase <github@eric.addymail.com>

--- public/script/VisualElementEditor.js
+++ public/script/VisualElementEditor.js
@@ -1,4 +1,4 @@
-import { CoordinateSpaceContainer, scalePoint } from './CoordinateSpaceContainer.js';
+import { CoordinateSpaceContainer, scalePoint, toPoint } from './CoordinateSpaceContainer.js';
 import { setupPointerEvents } from './PointerEvents.js';
 
 class VisualElementEditorHandle {
@@ -111,6 +111,68 @@ export class VisualElementEditor {
   }
 
   /**
+   * @param {number} scale
+   * @this VisualElementEditor
+   */
+  setScale(scale) {
+    this.scale = scale;
+  }
+
+  /**
+   * @param {object} delta
+   * @param {number} delta.x
+   * @param {number} delta.y
+   * @param {number} index
+   * @this VisualElementEditor
+   */
+  moveEdgeBy(delta, index) {
+    delta = scalePoint(this.scale, delta);
+
+    if (this.activeElement instanceof SVGRectElement) {
+      switch (index) {
+        case 1:
+          {
+            // left edge
+            const w = this.activeElement.width.baseVal.value;
+            const new_w = Math.max(10, w - delta.x);
+            this.activeElement.x.baseVal.value -= new_w - w;
+            this.activeElement.width.baseVal.value = new_w;
+          }
+          break;
+        case 2:
+          {
+            // top edge
+            const h = this.activeElement.height.baseVal.value;
+            const new_h = Math.max(10, h - delta.y);
+            this.activeElement.y.baseVal.value -= new_h - h;
+            this.activeElement.height.baseVal.value = new_h;
+          }
+          break;
+        case 3:
+          {
+            // right edge
+            const w = this.activeElement.width.baseVal.value;
+            const new_w = Math.max(10, w + delta.x);
+            // this.activeElement.x.baseVal.value -= new_w - w;
+            this.activeElement.width.baseVal.value = new_w;
+          }
+          break;
+        case 4:
+          {
+            // bottom edge
+            const h = this.activeElement.height.baseVal.value;
+            const new_h = Math.max(10, h + delta.y);
+            // this.activeElement.y.baseVal.value -= new_h - h;
+            this.activeElement.height.baseVal.value = new_h;
+          }
+          break;
+      }
+    }
+
+    this.updateHandles();
+  }
+
+  /**
    * @param {object} delta
    * @param {number} delta.x
    * @param {number} delta.y
@@ -179,9 +241,13 @@ export class VisualElementEditor {
     if (this.activeElement instanceof SVGRectElement) {
       const w = this.activeElement.width.baseVal.value;
       const h = this.activeElement.height.baseVal.value;
-      const [new_w, new_h] = getProportionateDimensions(delta, w, h);
-      this.activeElement.width.baseVal.value = new_w;
-      this.activeElement.height.baseVal.value = new_h;
+      const [new_w, new_h] = getProportionateDimensions(delta.x, w, h);
+      if (w !== new_w && h !== new_h) {
+        this.activeElement.width.baseVal.value = new_w;
+        this.activeElement.height.baseVal.value = new_h;
+        this.activeElement.x.baseVal.value -= (new_w - w) / 2;
+        this.activeElement.y.baseVal.value -= (new_h - h) / 2;
+      }
     }
     if (this.activeElement instanceof SVGPolygonElement) {
       let minX = this.activeElement.points[0].x;
@@ -196,7 +262,7 @@ export class VisualElementEditor {
       }
       const w = maxX - minX;
       const h = maxY - minY;
-      const [new_w, new_h] = getProportionateDimensions(delta, w, h);
+      const [new_w, new_h] = getProportionateDimensions(delta.x * 2, w, h);
       if (w !== new_w && h !== new_h) {
         for (const point of this.activeElement.points) {
           const ratioX = (point.x - minX) / w;
@@ -210,14 +276,6 @@ export class VisualElementEditor {
     this.updateHandles();
   }
 
-  /**
-   * @param {number} scale
-   * @this VisualElementEditor
-   */
-  setScale(scale) {
-    this.scale = scale;
-  }
-
   hideHandles() {
     for (const handle of this.handleList) {
       handle.hide();
@@ -228,6 +286,16 @@ export class VisualElementEditor {
     this.handleList[0].show((delta) => {
       this.resizeSelectedElementBy(delta);
     });
+    if (this.activeElement instanceof SVGRectElement) {
+      while (this.handleList.length < 1 + 4) {
+        this.handleList.push(new VisualElementEditorHandle(this.editorHandleContainer, this));
+      }
+      for (let i = 0; i < 1 + 4; i++) {
+        this.handleList[i].show((delta) => {
+          this.moveEdgeBy(delta, i);
+        });
+      }
+    }
     if (this.activeElement instanceof SVGPolygonElement) {
       while (this.handleList.length < 1 + this.activeElement.points.length) {
         this.handleList.push(new VisualElementEditorHandle(this.editorHandleContainer, this));
@@ -242,37 +310,55 @@ export class VisualElementEditor {
   }
 
   updateHandles() {
+    this.updateSizeHandle();
+
+    if (this.activeElement instanceof SVGRectElement) {
+      const point = this.coordinateSpace.childPointToGlobalPoint(this.activeElement, scalePoint(1 / this.scale, toPoint(this.activeElement.x.baseVal.value, this.activeElement.y.baseVal.value)));
+      const size = scalePoint(1 / this.scale, toPoint(this.activeElement.width.baseVal.value, this.activeElement.height.baseVal.value));
+      // left edge
+      this.handleList[1].element.style.left = `${point.x - VisualElementEditorHandle.Size / 2}px`;
+      this.handleList[1].element.style.top = `${point.y + (size.y - VisualElementEditorHandle.Size) / 2}px`;
+      // top edge
+      this.handleList[2].element.style.left = `${point.x + (size.x - VisualElementEditorHandle.Size) / 2}px`;
+      this.handleList[2].element.style.top = `${point.y - VisualElementEditorHandle.Size / 2}px`;
+      // right edge
+      this.handleList[3].element.style.top = `${point.y + (size.y - VisualElementEditorHandle.Size) / 2}px`;
+      this.handleList[3].element.style.left = `${point.x + size.x - VisualElementEditorHandle.Size / 2}px`;
+      // bottom edge
+      this.handleList[4].element.style.left = `${point.x + (size.x - VisualElementEditorHandle.Size) / 2}px`;
+      this.handleList[4].element.style.top = `${point.y + size.y - VisualElementEditorHandle.Size / 2}px`;
+    }
+    if (this.activeElement instanceof SVGPolygonElement) {
+      for (let i = 0; i < this.activeElement.points.length; i++) {
+        const point = this.coordinateSpace.childPointToGlobalPoint(this.activeElement, scalePoint(1 / this.scale, this.activeElement.points[i]));
+        this.handleList[1 + i].element.style.left = `${point.x - VisualElementEditorHandle.Size / 2}px`;
+        this.handleList[1 + i].element.style.top = `${point.y - VisualElementEditorHandle.Size / 2}px`;
+      }
+    }
+  }
+
+  updateSizeHandle() {
     const rect = this.getSelectedElementRect();
     if (rect) {
-      // update size handle
       this.handleList[0].element.style.left = `${rect.x + rect.width + VisualElementEditorHandle.Size}px`;
       this.handleList[0].element.style.top = `${rect.y + rect.height / 2 - VisualElementEditorHandle.Size / 2}px`;
-      if (this.activeElement instanceof SVGPolygonElement) {
-        for (let i = 0; i < this.activeElement.points.length; i++) {
-          const point = this.coordinateSpace.childPointToGlobalPoint(this.activeElement, scalePoint(1 / this.scale, this.activeElement.points[i]));
-          this.handleList[1 + i].element.style.left = `${point.x - VisualElementEditorHandle.Size / 2}px`;
-          this.handleList[1 + i].element.style.top = `${point.y - VisualElementEditorHandle.Size / 2}px`;
-        }
-      }
     }
   }
 }
 
 /**
- * @param {object} delta
- * @param {number} delta.x
- * @param {number} delta.y
+ * @param {number} delta
  * @param {number} w
  * @param {number} h
  */
 function getProportionateDimensions(delta, w, h) {
   const ratio = w / h;
   if (w < h) {
-    const new_w = Math.max(10, w + delta.x * 2);
+    const new_w = Math.max(10, w + delta);
     const new_h = new_w / ratio;
     return [new_w, new_h];
   } else {
-    const new_h = Math.max(10, h + delta.x * 2);
+    const new_h = Math.max(10, h + delta);
     const new_w = ratio * new_h;
     return [new_w, new_h];
   }
